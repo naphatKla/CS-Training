4f6ceb1e5887518197cb32a6967a5a3bb080be73
kla <klalnw2546@gmail.com>
2022-09-28 12:04:11 +0700

    update

--- Borntodev_Exercise/Exercise/Exercise/Program.cs
+++ Borntodev_Exercise/Exercise/Exercise/Program.cs
@@ -13,14 +13,14 @@ namespace Exercise
             // Use EX.Exercise_01 to show any exercsie ( 01 is number of exercise )
             
             RunEx();
-
+          
         }
 
         // devlab3 can't use any class because they need only 1 .cs file
         // so i have to create RunEx function
-        static void RunEx() 
+        static void RunEx()
         {
-            
+            Console.WriteLine(6 / 2 * (2 + 1));
         }
     }
 }
